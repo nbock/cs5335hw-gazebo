b6e03aa7eccc71013d94693b8743bf89252ebbda
Nat Tuck <nat@ferrus.net>
2020-10-09 11:37:29 -0400

    Tweak up mazegen for mapping hw.

--- hw06/mazegen/mazegen.rb
+++ hw06/mazegen/mazegen.rb
@@ -2,17 +2,6 @@
 
 require 'erb'
 
-class Grid
-  def initialize(size)
-    @cells = {}
-    0.upto(size-1) do |ii|
-      0.upto(size-1) do |jj|
-        @cells["#{ii},#{jj}"] =
-      end
-    end
-  end
-end
-
 class MazeGen
   def initialize()
     @wall_ii = 0
@@ -26,6 +15,7 @@ class MazeGen
 
   def wall(xx, yy, rot=false)
     aa = rot ? 0 : (Math::PI/2)
+    aa += 0.1 * (rand(7) - 3)
     %Q{
     <include>
       <uri>model://brick_box_3x1x3</uri>
@@ -39,6 +29,54 @@ class MazeGen
     1 + rand(4)
   end
 
+  def safe(xx, yy)
+    yy.abs > 2 || (20 - xx.abs).abs > 2
+  end
+
+  def split_y(walls, x0, y0, x1, y1)
+    dx = x1 - x0
+    dy = y1 - y0
+
+    if (dy < 10) then
+      return
+    end
+
+    wx = x0 + 6 + rand(dx - 6)
+    wy = y0 + 6 + rand(dy - 6)
+
+    jj = wy
+    x0.step(x1+2, 3) do |ii|
+      if ((ii - wx).abs >= 3) && safe(ii, jj) then
+        walls.push(wall(ii, jj, true))
+      end
+    end
+
+    split_x(walls, x0, y0, x1, wy - 2)
+    split_x(walls, x0, wy + 2, x1, y1)
+  end
+
+  def split_x(walls, x0, y0, x1, y1)
+    dx = x1 - x0
+    dy = y1 - y0
+
+    if (dx < 10) then
+      return
+    end
+
+    wx = x0 + 6 + rand(dx - 6)
+    wy = y0 + 6 + rand(dy - 6)
+
+    ii = wx
+    y0.step(y1+2, 3) do |jj|
+      if ((jj - wy).abs >= 3) && safe(ii, jj) then
+        walls.push(wall(ii, jj))
+      end
+    end
+
+    split_y(walls, x0, y0, wx - 2, y1)
+    split_y(walls, wx + 2, y0, x1, y1)
+  end
+
   def gen
     tpl = ERB.new(File.read("template.xml.erb"), trim_mode: "-<>")
     walls = []
@@ -49,6 +87,8 @@ class MazeGen
       walls.push(wall(-28, xx, false))
       walls.push(wall(28, xx, false))
     end
+
+    split_x(walls, -27, -27, 27, 27)
 
     puts tpl.run(binding)
   end
